dd97f104312ab8514026d45667582ff78a6c5b3c
John Hensley <hensley@umich.edu>
2016-09-21 15:35:04 -0400

    Correct ataqc dependencies

--- ataqc.rb
+++ ataqc.rb
@@ -4,6 +4,7 @@ class Ataqc < Formula
   url "https://github.com/ParkerLab/ataqc/archive/0.4.1.tar.gz"
   version "0.4.1"
   sha256 "57a828aecf6e0e237da721948b73a59f78b8c05fa5253b4467cb62473acff515"
+  depends_on "boost"
   depends_on "homebrew/science/htslib"
 
   def install
