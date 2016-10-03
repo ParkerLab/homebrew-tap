8dcb70382e95ca7cbed23f29ff53ad3f850ffa07
John Hensley <hensley@umich.edu>
2016-10-03 14:32:08 -0400

    ataqc 0.5.0

--- ataqc.rb
+++ ataqc.rb
@@ -1,13 +1,13 @@
 class Ataqc < Formula
   desc ""
   homepage "https://github.com/ParkerLab/ataqc"
-  url "https://github.com/ParkerLab/ataqc/archive/0.4.1.tar.gz"
-  version "0.4.1"
-  sha256 "57a828aecf6e0e237da721948b73a59f78b8c05fa5253b4467cb62473acff515"
+  url "https://github.com/ParkerLab/ataqc/archive/0.5.0.tar.gz"
+  version "0.5.0"
+  sha256 "643e92e34777c3826bd8736ce212b5f491686e15ed995df7c40cf7ef7227daab"
   depends_on "boost"
   depends_on "homebrew/science/htslib"
 
   def install
-    system "make", "install", "PREFIX=#{HOMEBREW_PREFIX}/Cellar/ataqc/0.4.1/"
+    system "make", "install", "PREFIX=#{HOMEBREW_PREFIX}/Cellar/ataqc/0.5.0/"
   end
 end
