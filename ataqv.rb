33ea20e1e6bca2631f98b14c597deb224097930a
John Hensley <hensley@umich.edu>
2017-01-20 13:46:38 -0500

    ataqv 0.6.1

--- ataqv.rb
+++ ataqv.rb
@@ -1,13 +1,13 @@
 class Ataqv < Formula
   desc ""
   homepage "https://github.com/ParkerLab/ataqv"
-  url "https://github.com/ParkerLab/ataqv/archive/0.6.0.tar.gz"
-  version "0.6.0"
-  sha256 "ec9c275fb9bdc8e78f648b3fee0150b2ed35939e4a8c4860c75c652a23ebf799"
+  url "https://github.com/ParkerLab/ataqv/archive/0.6.1.tar.gz"
+  version "0.6.1"
+  sha256 "a9342ae94b99b3658cd1cdfb6b4612dc29dc317608bcda173cd12b560cf61b8f"
   depends_on "boost"
   depends_on "homebrew/science/htslib"
 
   def install
-    system "make", "install", "PREFIX=#{HOMEBREW_PREFIX}/Cellar/ataqv/0.6.0/"
+    system "make", "install", "PREFIX=#{HOMEBREW_PREFIX}/Cellar/ataqv/0.6.1/"
   end
 end
