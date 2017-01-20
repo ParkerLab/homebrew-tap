82f33e2fb0b361035489043bc7d3eb67a5663971
John Hensley <hensley@umich.edu>
2017-01-20 12:33:05 -0500

    Fix ataqv hash

--- ataqv.rb
+++ ataqv.rb
@@ -3,7 +3,7 @@ class Ataqv < Formula
   homepage "https://github.com/ParkerLab/ataqv"
   url "https://github.com/ParkerLab/ataqv/archive/0.6.0.tar.gz"
   version "0.6.0"
-  sha256 "5e7e75de5fdd02bdec25c20909632f7733c6e54c1d32e9b92ee06bcf3c8be5ce"
+  sha256 "ec9c275fb9bdc8e78f648b3fee0150b2ed35939e4a8c4860c75c652a23ebf799"
   depends_on "boost"
   depends_on "homebrew/science/htslib"
 
