1471175f6d7ab66ec62ca5b5e67c0434317ef36d
John Hensley <hensley@umich.edu>
2017-08-18 15:32:13 -0400

    ataqv 0.9.5

--- ataqv.rb
+++ ataqv.rb
@@ -1,14 +1,14 @@
 class Ataqv < Formula
   desc ""
   homepage "https://github.com/ParkerLab/ataqv"
-  url "https://github.com/ParkerLab/ataqv/archive/0.9.4.tar.gz"
-  version "0.9.4"
-  sha256 "85cccf762339ffd62db5e985eadb1541cee23e522de9d8fdab7eb1e9228fc725"
+  url "https://github.com/ParkerLab/ataqv/archive/0.9.5.tar.gz"
+  version "0.9.5"
+  sha256 "ccad095682232a0361f314013de822ccf1696db47dc2804880dd32ee4d5a1254"
   depends_on "coreutils"
   depends_on "boost"
   depends_on "homebrew/science/htslib"
 
   def install
-    system "make", "install", "prefix=#{HOMEBREW_PREFIX}/Cellar/ataqv/0.9.4/"
+    system "make", "install", "prefix=#{HOMEBREW_PREFIX}/Cellar/ataqv/0.9.5/"
   end
 end
